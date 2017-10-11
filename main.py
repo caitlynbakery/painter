3d3e9e43677ae5141f62540d911f89974e61bf86
Caitlyn <craigoda+caitlynbakery@gmail.com>
2017-10-11 07:27:01 -0700

    add eraser

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 
-size = (800, 600)
+screenwidth = 1280
+screenheight = 720
+size = (screenwidth, screenheight)
 screen = pygame.display.set_mode(size)
 
 pink = (252, 151, 240)
+white = (255, 255, 255)
+
+drawingRect = pygame.Rect(200, 0, screenwidth-200, screenheight)
+pygame.draw.rect(screen, white, drawingRect)
+
+eraser = pygame.image.load("img/eraser.png")
+eraserRect = pygame.Rect(20, 20, 80, 80)
+pinkRect = pygame.Rect(120, 20, 50, 50)
+pygame.draw.rect(screen, pink, pinkRect)
+brushColor = pink
 
 gameon = True
 
 mousedown = False
+
 while gameon:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,6 +30,12 @@ while gameon:
         if event.type == pygame.MOUSEBUTTONUP:
             mousedown = False
         mousepos = pygame.mouse.get_pos()
+    screen.blit(eraser, eraserRect)
     if mousedown == True:
-        pygame.draw.circle(screen, pink, mousepos, 10)
+        if drawingRect.collidepoint(mousepos):
+            pygame.draw.circle(screen, brushColor, mousepos, 10)
+        if eraserRect.collidepoint(mousepos):
+            brushColor = white
+        if pinkRect.collidepoint(mousepos):
+            brushColor = pink
     pygame.display.update()
